cc51ea3fd83431535375942de484d695c583ff04
shreyanshchauhan <shreyansh0514@gmail.com>
2025-04-09 11:36:42 +0530

    Update backend

--- Day5/frontend/src/components/View.jsx
+++ Day5/frontend/src/components/View.jsx
@@ -6,7 +6,7 @@ const View = () => {
     viewdata();
   },[])
   const viewdata= async()=>{
-    const res=await axios.get('http://localhost:9000/users');
+    const res=await axios.get('https://user-registration-system-ms1q.onrender.com/users');
     console.log(res);
     setUsers(res.data);
   }
